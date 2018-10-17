e9d4d710cee3e88c0e5121a05aff2fb89a30c243
MERCIER SAMUEL (BROSS1915) <SMERCI07@saturn.dkcorp.net>
2018-10-17 12:58:07 -0400

    added rescaling

--- src/extract_images.py
+++ src/extract_images.py
@@ -12,15 +12,31 @@ import json
 import os
 import pandas as pd
 from selenium import webdriver
+import shutil
 from urllib.request import Request, urlopen
 
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 
 class extract_images():
+    
+    def __init__(self):
+        pass
+    
+    #method to delete images in a folder
+    def _delete_images(self, folder):
+        if os.path.exists(folder):
+            for the_file in os.listdir(folder):
+                file_path = os.path.join(folder, the_file)
+                try:
+                    if os.path.isfile(file_path):
+                        os.unlink(file_path)
+                    elif os.path.isdir(file_path): shutil.rmtree(file_path)
+                except Exception as e:
+                    print(e)
       
-    #function to extract images for a given search term
-    def _get_images(self,search_term, nb_images, verbose, browser, header):
+    #method to extract images for a given search term
+    def _get_images(self,search_term, nb_images, folder_name, level, verbose, browser, header):
         url = "https://www.google.co.in/search?q="+search_term+"&source=lnms&tbm=isch"
         browser.get(url)
         
@@ -46,10 +62,11 @@ class extract_images():
             try:
                 req = Request(img)
                 raw_img = urlopen(req, timeout=10).read()
-                File = open(os.path.join(parentdir + '/data/image_dataset/train/' + search_term, search_term + "_" + str(counter) + "." + imgtype), "wb")
+                File = open(os.path.join(parentdir + '/data/image_dataset/' + level + '/' + folder_name, folder_name + "_" + str(self.success_dict[folder_name]) + "." + imgtype), "wb")
                 File.write(raw_img)
                 File.close()
                 success = success + 1
+                self.success_dict[folder_name] += 1             
             except:
                     pass
                 
@@ -59,16 +76,44 @@ class extract_images():
         print(success, "pictures succesfully downloaded")
         browser.close() 
         
-    #main function: runs through search terms, and extract images 
-    def run(self, search_terms, nb_images=100, delete_previous_images=True, 
+    #main method: runs through search terms, and extract images 
+    def run(self, nb_images=100, delete_previous_images=True, 
             path_to_driver=None, verbose=True):
         """
-        search_terms: list of terms we want to search
+        search_terms: list of terms we want to search, along with the name of the search folder, if it is for training or validation, and the number of images we want to extract
         nb_images: number of images we want to extract
         delete_previous_images: if we want to empty the folder before the search, to avoid duplicate images
         path_to_driver: path to the chrome driver; if None, we assume it is in the root folder
         """
         
+        #read the search terms csv
+        search_terms_df = pd.read_csv(parentdir + '/data/searchterms.csv', delimiter=',')              
+        search_terms = search_terms_df['search_term'].unique()
+        self.success_dict = {v:0 for v in search_terms_df.folder_name.unique()}
+        
+        #if we want to delete previously extracted images, to avoid duplicates
+        if delete_previous_images:
+            print('Deleting previous images')            
+            #training set
+            folder = parentdir + '/data/image_dataset/train'
+            self._delete_images(folder)
+            #validation set
+            folder = parentdir + '/data/image_dataset/val'
+            self._delete_images(folder)
+            #augmented set
+            folder = parentdir + '/data/augmented_dataset'
+            self._delete_images(folder)
+                    
+        #create the name of the folders
+        for name in search_terms_df['folder_name'].unique():
+            folder = parentdir + '/data/image_dataset/train/' + name
+            if not os.path.exists(folder):
+                os.mkdir(parentdir + '/data/image_dataset/train/' + name)
+            folder = parentdir + '/data/image_dataset/val/' + name
+            if not os.path.exists(folder):
+                os.mkdir(parentdir + '/data/image_dataset/val/' + name)
+        
+        #extract the images
         for search_term in search_terms:           
             #we launch Chrome using selenium
             #if the path of the chrome driver is provided, we use it; otherwise, we assume it is in the root folder
@@ -78,31 +123,16 @@ class extract_images():
                 browser = webdriver.Chrome(parentdir + '/chromedriver.exe') 
                 
             header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
-            
-        
-            folder = parentdir + '/data/image_dataset/train/' + search_term
-            if os.path.exists(folder):
-                #if we want to delete previously extracted images, to avoid duplicates
-                if delete_previous_images:
-                    print('Deleting previous images')
-                    for file in os.listdir(folder):
-                        try:
-                            file_path = os.path.join(folder, file)
-                            os.unlink(file_path) 
-                        except:
-                            print('Cannot delete file', '/data/image_dataset/train/' + search_term)
-            #if no folder with the name = search term exists, create it        
-            else:
-                os.mkdir(parentdir + '/data/image_dataset/train/' + search_term)
-                
-            #extract the images
-            self._get_images(search_term, nb_images=nb_images, verbose=verbose, browser=browser, header=header)                   
+                          
+            nb_images = search_terms_df.loc[search_terms_df.search_term==search_term, 'number_imgs'].values[0]
+            level = search_terms_df.loc[search_terms_df.search_term==search_term, 'train_val'].values[0]
+            folder_name = search_terms_df.loc[search_terms_df.search_term==search_term, 'folder_name'].values[0]
+            self._get_images(search_term, nb_images=nb_images, folder_name=folder_name, level=level, verbose=verbose, browser=browser, header=header)                   
             del browser
+
 
 if __name__ == '__main__':
     #extract search terms
-    search_terms = list(pd.read_csv(parentdir + '/data/searchterms.csv', delimiter=',').columns)
-#    search_terms = ['hockey sticks', 'hockey helmet', 'hockey puck', 'hockey skates', 'hockey pants', 'hockey gloves']
     extracter = extract_images()
-    extracter.run(search_terms, nb_images=120, verbose=True)
+    extracter.run(verbose=True)
     
--- src/image_classifier.py
+++ src/image_classifier.py
@@ -10,14 +10,13 @@ images (using imagenetscraper) and ensemble a number of models
 @author: sam
 """
 
-import matplotlib.pyplot as plt
-import numpy as np
-import os
+import dill
+import pickle
 
 from tensorflow.python.keras.applications.inception_v3 import InceptionV3, decode_predictions, preprocess_input
 from tensorflow.python.keras.preprocessing import image
 from tensorflow.python.keras.models import Model, Sequential
-from tensorflow.python.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.python.keras.layers import Dense, GlobalAveragePooling2D, Dropout
 from tensorflow.python.keras.optimizers import Adam
 from tensorflow.python.keras.preprocessing.image import ImageDataGenerator, img_to_array
 from tensorflow.python.keras import backend as K
@@ -26,9 +25,10 @@ import os,sys,inspect
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 
-TRAIN_DIR = parentdir + '/Data/image_dataset/train'
-TEST_DIR = parentdir + '/Data/image_dataset/test'
-AUGMENTED_DIR = '/Data/image_dataset/augmented_dataset'
+TRAIN_DIR = parentdir + '/data/image_dataset/train'
+VAL_DIR = parentdir + '/data/image_dataset/val'
+TEST_DIR = parentdir + '/data/image_dataset/test'
+AUGMENTED_DIR = parentdir + '/data/augmented_dataset'
 
 class image_classifier():
     
@@ -36,110 +36,72 @@ class image_classifier():
         pass
     
     #optimize the hyperparameters of the model        
-    def _hyperparameter_optimization(self, num_iterations=50,
-                                     max_nrows=None, num_threads=1, calculated_sports=True,
-                                     display_plot=False, test_percentage=0.2, k=5,
-                                     specific_countries=None, predict_user_ids=None,
-                                     save_results=False, max_users=None):
+    def _hyperparameter_optimization(self, num_iterations=50, save_results=True,
+                                     display_plot=False):
         """
-        fitness_function: 0-->built-in precision+recall, 1-->built-in precision,
-                          2-->built-in recall, 3-->manual recall function
+        num_iterations: number of hyperparameter combinations we try
         """
-        
-        #The choice of the fitness function affects the construction of the test set
-        if fitness_function == 3:
-            USE_BUILT_IN_STATS=False
-        else:
-            USE_BUILT_IN_STATS=True
-        
-        self.hyperparameter_opt_conditions = {
-                'num_threads': num_threads,
-                'test_percentage': test_percentage,
-                'use_built_in_stats': USE_BUILT_IN_STATS
-                }
         
         #import scikit-optimize libraries
         from skopt import gp_minimize
         from skopt.space import Real, Categorical, Integer
         from skopt.plots import plot_convergence
         from skopt.utils import use_named_args
-        
-        #read the data
-        self.countries, self.data = utils.read_data_S3(calculated_sports=calculated_sports, 
-                                                       max_nrows=max_nrows,
-                                                       specific_countries=specific_countries)
-        
+                
         #declare the hyperparameters search space
-        dim_num_components = Integer(low=1, high=60, name='num_components')
-        dim_epochs = Integer(low=1, high=20, name='epochs')
-        dim_loss = Categorical(categories=['warp', 'bpr'], name='loss')      
-        dim_learning_rate = Real(low=1e-3, high=1e-1, prior='log-uniform',
+        dim_epochs = Integer(low=1, high=10, name='epochs')
+        dim_hidden_size = Integer(low=3, high=1000, name='hidden_size')   
+        dim_learning_rate = Real(low=1e-6, high=1e-2, prior='log-uniform',
                                  name='learning_rate')
-        dim_user_alpha = Real(low=0, high=1e-3, name='user_alpha')
-        dim_include_user_features = Categorical(categories=[True, False], 
-                                                 name='include_user_features')
-        dim_add_user_identity = Categorical(categories=[True, False], 
-                                            name='add_user_identity')
+        dim_dropout = Real(low=0, high=0.9, name='dropout')
+        dim_fine_tuning = Categorical(categories=[True, False], name='fine_tuning')
+        dim_nb_layers = Integer(low=1, high=3, name='nb_layers')   
+        dim_activation = Categorical(categories=['relu', 'tanh'], name='activation')
 
-        dimensions = [dim_num_components,
-                      dim_epochs,
-                      dim_loss,
+        dimensions = [dim_epochs,
+                      dim_hidden_size,
                       dim_learning_rate,
-                      dim_user_alpha,
-                      dim_include_user_features,
-                      dim_add_user_identity]
+                      dim_dropout,
+                      dim_fine_tuning,
+                      dim_nb_layers,
+                      dim_activation]
         
         #read default parameters from last optimization
         try:
-            with open(parentdir + '/data/trained_model/hyperparameters_search' + ''.join(['_' + i for i in specific_countries]) + '.pickle', 'rb') as f:
+            with open(parentdir + '/data/trained_model/hyperparameters_search.pickle', 'rb') as f:
                 sr = dill.load(f)
             default_parameters = sr.x
             print('parameters of previous optimization loaded!')
 
         except:
             #fall back default values
-            default_parameters = [2, 5, 'warp', 0.05, 0.0, True, False]
+            default_parameters = [5, 1024, 1e-4, 0, False, 1, 'relu']
         
         self.number_iterations = 0
     
         #declare the fitness function
         @use_named_args(dimensions=dimensions)
-        def fitness(num_components, epochs, loss, learning_rate, user_alpha, 
-                    include_user_features, add_user_identity):
+        def fitness(epochs, hidden_size, learning_rate, dropout, 
+                    fine_tuning, nb_layers, activation):
             
             self.number_iterations += 1
             
             #print the hyper-parameters            
-            print('num components:', num_components)
             print('epochs:', epochs)
-            print('loss:', loss)
+            print('hidden_size:', hidden_size)
             print('learning rate:', learning_rate)
-            print('user alpha:', user_alpha)
-            print('include user features:', include_user_features)
-            print('add user identity:', add_user_identity)
+            print('dropout:', dropout)
+            print('fine_tuning:', fine_tuning)
+            print('nb_layers:', nb_layers)
+            print('activation:', activation)
             print()
             
             #fit the model
-            self.fit(num_components=num_components, epochs=epochs, loss=loss, 
-                     learning_rate=learning_rate, user_alpha=user_alpha,
-                     include_user_features=include_user_features,
-                     add_user_identity=add_user_identity,
-                     load_data=False, **self.hyperparameter_opt_conditions)
+            self.fit(epochs=epochs, hidden_size=hidden_size, learning_rate=learning_rate, dropout=dropout, 
+                    fine_tuning=fine_tuning, nb_layers=nb_layers, activation=activation)
             
-            #calculate fitness
-            if fitness_function==0: #sum of built-in precision and fitness
-                self._precision_at_k(k=k)
-                self._recall_at_k(k=k)
-                fitness = self.precision + self.recall
-            elif fitness_function==1: #built-in precision
-                self._precision_at_k(k=k)
-                fitness = self.precision
-            elif fitness_function==2: #built-in recall
-                self._recall_at_k(k=k)
-                fitness = self.recall
-            elif fitness_function==3: #manual recall function
-                self._statistics_at_k(k=k, calculate_precision=False, calculate_coverage=False, max_users=max_users)
-                fitness = self.recall
+            #extract fitness
+            fitness = self.history['val_accuracy']
                 
             print('CALCULATED FITNESS AT ITERATION', self.number_iterations, 'OF:', fitness)
             print()
@@ -157,10 +119,10 @@ class image_classifier():
                             x0=default_parameters)
         
         if save_results:
-            with open(parentdir + '/data/trained_model/hyperparameters_dimensions' + ''.join(['_' + i for i in specific_countries]) + '.pickle', 'wb') as f:
+            with open(parentdir + '/data/trained_model/hyperparameters_dimensions.pickle', 'wb') as f:
                 dill.dump(dimensions, f, protocol=pickle.HIGHEST_PROTOCOL)
             
-            with open(parentdir + '/data/trained_model/hyperparameters_search' + ''.join(['_' + i for i in specific_countries]) + '.pickle', 'wb') as f:
+            with open(parentdir + '/data/trained_model/hyperparameters_search.pickle', 'wb') as f:
                 dill.dump(self.search_result, f, protocol=pickle.HIGHEST_PROTOCOL)
             
             print("Hyperparameter search saved!")
@@ -176,11 +138,13 @@ class image_classifier():
         print('Optimal fitness value of:', -float(self.search_result.fun))
     
     #we fit the model given the images in the training set
-    def fit(self, learning_rate=1e-4, epochs=5, steps_per_epoch=20, 
-            save_augmented=False, batch_size=20, save_model=True, verbose=True,
+    def fit(self, learning_rate=1e-4, epochs=5, activation='relu',
+            dropout=0, hidden_size=1024, nb_layers=1, steps_per_epoch=40,
+            val_steps_per_epoch=5, save_augmented=False, 
+            batch_size=20, save_model=True, verbose=True,
             fine_tuning=False, NB_IV3_LAYERS_TO_FREEZE=279):
         
-        #Load the pretrained model, withoug the classification (top) layers
+        #load the pretrained model, withoug the classification (top) layers
         base_model = InceptionV3(weights='imagenet', include_top=False)
         
         #We expect the classes to be the name of the folders in the training set
@@ -189,7 +153,11 @@ class image_classifier():
         #Add the classification layers using Keras functional API
         x = base_model.output
         x = GlobalAveragePooling2D()(x)
-        x = Dense(1024, activation='relu')(x) #Hidden layer for classification
+        for _ in range(nb_layers):
+            x = Dense(hidden_size, activation=activation)(x) #Hidden layer for classification
+            if dropout > 0:
+                x = Dropout(rate=dropout)(x)
+            
         predictions = Dense(len(self.categories), activation='softmax')(x) #Output layer
         model = Model(inputs=base_model.input, outputs=predictions)
         
@@ -207,6 +175,7 @@ class image_classifier():
         
         #Define the dataset augmentation and batch generator
         datagen_train = ImageDataGenerator(rotation_range=180,
+                                           rescale=1./255,
                                            width_shift_range=0.1,
                                            height_shift_range=0.1,
                                            shear_range=0.1,
@@ -215,6 +184,8 @@ class image_classifier():
                                            vertical_flip=True,
                                            fill_mode='nearest'
                                            )
+                
+        datagen_val = ImageDataGenerator(rescale=1./255)
         
         #Save the augmented images if we want to
         if save_augmented:
@@ -222,17 +193,24 @@ class image_classifier():
         else:
             save_to_dir = None
     
-        generator_train = datagen_train.flow_from_directory(directory=TRAIN_DIR,
+        self.generator_train = datagen_train.flow_from_directory(directory=TRAIN_DIR,
                                                             target_size=(299, 299),
                                                             batch_size=batch_size,
                                                             shuffle=True,
                                                             save_to_dir=save_to_dir)
         
+        self.generator_val = datagen_val.flow_from_directory(directory=VAL_DIR,
+                                                            target_size=(299, 299),
+                                                            batch_size=batch_size,
+                                                            shuffle=False)
+        
         #Fit the model
-        history = model.fit_generator(generator=generator_train,
+        self.history = model.fit_generator(generator=self.generator_train,
                                   epochs=epochs,
                                   steps_per_epoch=steps_per_epoch,
-                                  verbose=verbose)
+                                  verbose=verbose,
+                                  validation_data=self.generator_val,
+                                  validation_steps=val_steps_per_epoch)
         
         #Fine-tune the model, if we wish so
         if fine_tuning:
@@ -241,15 +219,23 @@ class image_classifier():
                 layer.trainable = False
             for layer in model.layers[NB_IV3_LAYERS_TO_FREEZE:]:
                 layer.trainable = True
-            model.compile(optimizer=Adam(lr=learning_rate/0.1),   
+            model.compile(optimizer=Adam(lr=learning_rate*0.1),   
                           loss=loss,
-                          metrics=['categorical_accuracy'])
+                          metrics=['accuracy'])
             
             #Fit the model
-            history = model.fit_generator(generator=generator_train,
+            self.history = model.fit_generator(generator=self.generator_train,
                                       epochs=epochs,
                                       steps_per_epoch=steps_per_epoch,
-                                      verbose=verbose)
+                                      verbose=verbose,
+                                      validation_data=self.generator_val,
+                                      validation_steps=val_steps_per_epoch)
+            
+        #Evaluate the model, just to be sure
+        self.generator_train.reset()
+        self.results = model.evaluate_generator(generator=self.generator_train, steps=steps_per_epoch)
+        print(self.results[1])
+            
         
         #Save the model
         if save_model:
@@ -259,22 +245,21 @@ class image_classifier():
         self.model = model
             
     #evaluation of the accuracy of classification on the test set
-    def predict(self):
-        #load the model
-        
-        
-        path_to_test = '..\\Test_images'
-        generator_test = ImageDataGenerator().flow_from_directory(directory=path_to_test,
+    def predict(self, path):
+        generator_test = ImageDataGenerator().flow_from_directory(directory=path,
                                          target_size=(299, 299),
                                          shuffle=False)
         
         #results = model.predict_generator(generator=generator_test)
-        test_results = self.model.evaluate_generator(generator=generator_test)
-        print('The test accuracy is of', test_results[1]*100, '%')
+        self.test_results = self.model.evaluate_generator(generator=generator_test)
+        print('The test accuracy is of', self.test_results[1]*100, '%')
+
         
 if __name__ == '__main__':
     classifier = image_classifier()
-    classifier.fit(fine_tuning=True, save_model=False)
+    classifier.fit(fine_tuning=False, save_model=False, epochs=5, steps_per_epoch=20,
+                   val_steps_per_epoch=3, save_augmented=False)
+#    classifier.predict(VAL_DIR)
         
         
         
